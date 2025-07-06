bcfed7f0dd436f7ed2c141e76280f69adf6e8f42
iamamy07 <ranjanamit219@gmail.com>
2025-07-06 09:06:43 +0530

    Added url in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  giftshopApiUrl: 'https://giftshop-backend-dx8s.onrender.com/api',
+  stripePublishableKey: 'pk_test_51RfwNMDHqNuUO8lE5y83go1nFq0LLWBD0batsXnrqAVj0mFFhVb9PNqtnswWjPggV0XI0jAmZpEgtqsfdAB1Rurk00y6ZVNKWH'
+
 };
