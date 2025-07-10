c4ccb4049cf9587bf7abdc9d3eb60fa6e4b5d973
Amit Ranjan <amitranjan@MacBookAir.lan>
2025-07-11 00:37:38 +0530

    some basic changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,4 @@ export const environment = {
   production: true,
   giftshopApiUrl: 'https://giftshop-backend-dx8s.onrender.com/api',
   stripePublishableKey: 'pk_test_51RfwNMDHqNuUO8lE5y83go1nFq0LLWBD0batsXnrqAVj0mFFhVb9PNqtnswWjPggV0XI0jAmZpEgtqsfdAB1Rurk00y6ZVNKWH'
-
 };
