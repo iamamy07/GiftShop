18bb92678372e78c5a7b06974b9e7fa1cdd4c582
Amit Ranjan <amitranjan@MacBookAir.lan>
2025-07-11 00:59:19 +0530

    fixing okta login issues

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,8 +1,19 @@
+// export default {
+//   oidc: {
+//     clientId: '0oaowl5n40f0VbDi95d7',
+//     issuer: 'https://dev-26468971.okta.com/oauth2/default',
+//     redirectUri: 'https://localhost:4200/login/callback',
+//     scopes: ['openid', 'profile', 'email']
+//   }
+// }
+
 export default {
   oidc: {
     clientId: '0oaowl5n40f0VbDi95d7',
     issuer: 'https://dev-26468971.okta.com/oauth2/default',
     redirectUri: 'https://localhost:4200/login/callback',
-    scopes: ['openid', 'profile', 'email']
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,                          // ✅ required for SPA
+    useInteractionCodeFlow: false       // ✅ disables IDX-only feature
   }
 }
